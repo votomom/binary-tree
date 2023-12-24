04b6d9b2f6967d2ab62311fd69a0c7cfd75f8b96
votomom <129229808+votomom@users.noreply.github.com>
2023-12-24 18:46:46 +0300

    final Update tree.hpp

--- tree/tree.hpp
+++ tree/tree.hpp
@@ -1,14 +1,18 @@
 #include <memory>
 
 struct tree_node {
-  int value;
-  tree_node *up;
-  std::unique_ptr<tree_node> left, right;
+    int value;
+    tree_node *up;
+    std::unique_ptr<tree_node> left, right;
+
+    bool is_leaf() const { return !left && !right; }
+    bool is_left() const { return up->left.get() == this; }
+    bool is_right() const { return up->right.get() == this; }
 };
 
 struct tree {
-  std::unique_ptr<tree_node> root;
+    std::unique_ptr<tree_node> root;
 
-  auto insert(int val) -> tree_node *;
-  auto remove(int val) -> bool;
+    auto insert(int val) -> tree_node *;
+    auto remove(int val) -> bool;
 };
